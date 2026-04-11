994f8ae6fa6f98468ecbde1d5ca1f7f40e9bf439
Falrafa4 <rafaasad9@gmail.com>
2026-04-11 17:20:37 +0700

    feat: initial commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <form className='login'>
+      <div className="login-header">
+        <h1>Login</h1>
+        <p>Selamat Datang di Spentaru Archive! Silahkan login.</p>
+      </div>
+    </form>
+  )
+}
