26c3d179663bf5b025a9a3debdc5b96dada9cf30
Falrafa4 <rafaasad9@gmail.com>
2026-04-11 20:43:01 +0700

    feat: add BaseLayout component with sidebar and main content structure

---
+++ src/layouts/BaseLayout.jsx
@@ -0,0 +1,16 @@
+import AppSidebar from "@/components/AppSidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import React from "react";
+import { Outlet } from "react-router";
+
+export default function BaseLayout() {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="p-5 md:p-10">
+        <SidebarTrigger />
+        <Outlet />
+      </main>
+    </SidebarProvider>
+  );
+}
