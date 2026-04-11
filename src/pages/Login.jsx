34f345fb327bce845f2544765c1d984abad8d1f2
Falrafa4 <rafaasad9@gmail.com>
2026-04-11 19:18:17 +0700

    feat: enhance Login component with Card layout and form elements

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,54 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React from "react";
 
 export default function Login() {
   return (
-    <form className='login'>
-      <div className="login-header">
-        <h1>Login</h1>
-        <p>Selamat Datang di Spentaru Archive! Silahkan login.</p>
-      </div>
+    <form className="flex justify-center items-center h-dvh">
+      <Card className="max-w-md w-full">
+        <CardHeader>
+          <CardTitle className="text-3xl">Login</CardTitle>
+          <CardDescription>
+            Selamat Datang di Spentaru Archive! Silahkan login.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="Masukkan password"
+                required
+              />
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </CardFooter>
+      </Card>
     </form>
-  )
+  );
 }
