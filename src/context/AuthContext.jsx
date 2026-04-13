3f74c8620918306db1cd03688c779f086183f9bf
Falrafa4 <rafaasad9@gmail.com>
2026-04-13 19:25:15 +0700

    feat: add AuthContext and useAuth hook for authentication management

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react-refresh/only-export-components */
+import { me } from "@/services/auth.service";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const res = await me();
+        if (res.data.status) {
+          setUser(res.data.data);
+        }
+      } catch (err) {
+        console.log(err.response);
+        localStorage.removeItem("token");
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    verifyToken();
+  }, []);
+
+  const login = (data) => {
+    localStorage.setItem("token", data.token);
+    setUser(data);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
